ec16f36074f481a1a4c2ede6b4e2de33b06141cb
Dragneel0112 <pillayruven@gmail.com>
2023-09-05 11:48:12 +0000

    Added files for Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 '''
 Basic Authentication
 '''
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -26,3 +27,22 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        '''
+        Decodes the Base64 string int Authorization header
+        Args:
+            base64_authorization_header: auth header with Base64 str
+        Returns: Decoded Base64 string
+        '''
+        if not (base64_authorization_header and
+                isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except BaseException:
+            return None
