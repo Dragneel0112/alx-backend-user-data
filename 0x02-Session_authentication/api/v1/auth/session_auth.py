5acad1300925efeb1009ab5d2304eb7c5d422c29
Dragneel0112 <pillayruven@gmail.com>
2023-09-06 10:14:08 +0000

    Updated session_auth.y for session id as required in Task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,20 @@
 Authenticates a session with Basic Authentication
 '''
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     '''
     Class for session authentication
     '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''
+        Creates session with unique id for the user
+        '''
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
