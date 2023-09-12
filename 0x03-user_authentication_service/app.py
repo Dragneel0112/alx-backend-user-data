dbae9c51d1a6d3e4c51f5a78a0a8375aa3440e80
Dragneel0112 <pillayruven@gmail.com>
2023-09-12 10:45:18 +0000

    Updated app.py as per Task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 '''
 A simple Flask app for Authentication
 '''
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
@@ -14,6 +16,20 @@ def index() -> str:
     Uses get route to return simple JSON payload
     '''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    '''
+    Uses POST requests to create Users
+    - Returns 400 error if User exists
+    '''
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
