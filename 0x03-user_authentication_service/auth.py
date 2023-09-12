c824129dd61ce9d886689073e384c0dc667a5d3f
Dragneel0112 <pillayruven@gmail.com>
2023-09-12 12:07:51 +0000

    Updated auth.py for Task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,6 +103,24 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        '''
+        Updates User password if reset token is valid
+        '''
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        new_password_hash = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=new_password_hash,
+            reset_token=None,
+        )
+
 
 def _generate_uuid() -> str:
     '''
